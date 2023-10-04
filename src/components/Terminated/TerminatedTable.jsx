6a424eb0430d7b3d39d773bbfb86699405d5b1c4
Simon Demazière <simon.demaziere@insee.fr>
2023-10-04 16:57:17 +0200

    Fix/buggy address and open terminated su questionnaire (#83) (#84)
    * fix: provide questionnaireId in queen URL
    fix : handle buggy address
    
    * fix: handle missing departement for export
    
    * fix: unnecessary script option
    
    * fix: update test

--- src/components/Terminated/TerminatedTable.jsx
+++ src/components/Terminated/TerminatedTable.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import Table from 'react-bootstrap/Table';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
-import displayStateHistoryTable from './DisplayStateHistoryTable';
-import PaginationNav from '../PaginationNav/PaginationNav';
-import SearchField from '../SearchField/SearchField';
-import SortIcon from '../SortIcon/SortIcon';
-import Utils from '../../utils/Utils';
-import D from '../../i18n';
-import './Terminated.css';
+import React from "react";
+import Table from "react-bootstrap/Table";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import displayStateHistoryTable from "./DisplayStateHistoryTable";
+import PaginationNav from "../PaginationNav/PaginationNav";
+import SearchField from "../SearchField/SearchField";
+import SortIcon from "../SortIcon/SortIcon";
+import Utils from "../../utils/Utils";
+import D from "../../i18n";
+import "./Terminated.css";
 
 class TerminatedTable extends React.Component {
   constructor(props) {
@@ -23,21 +23,21 @@ class TerminatedTable extends React.Component {
       displayData: props.data,
       toggleStateHistory: false,
       stateData: [],
-      stateId: '',
+      stateId: "",
       showComment: false,
-      suToModifySelected: '',
-      oldComment: '',
-      newComment: '',
-
+      suToModifySelected: "",
+      oldComment: "",
+      newComment: "",
     };
-    this.queenUrl = `${window.localStorage.getItem('QUEEN_URL_FRONT_END')}`;
+    this.queenUrl = `${window.localStorage.getItem("QUEEN_URL_FRONT_END")}`;
   }
 
   getMaxWidth() {
     return [
-      document.getElementById('stateHistoryDate').getBoundingClientRect().width,
-      document.getElementById('stateHistoryHour').getBoundingClientRect().width,
-      document.getElementById('stateHistoryState').getBoundingClientRect().width,
+      document.getElementById("stateHistoryDate").getBoundingClientRect().width,
+      document.getElementById("stateHistoryHour").getBoundingClientRect().width,
+      document.getElementById("stateHistoryState").getBoundingClientRect()
+        .width,
     ];
   }
 
@@ -64,14 +64,14 @@ class TerminatedTable extends React.Component {
   handleShowComment(line) {
     this.setState({ showComment: true, suToModifySelected: line.id });
     if (line.comments != null) {
-      let comToSet = '';
-      const comment = line.comments.find((c) => c.type === 'MANAGEMENT');
+      let comToSet = "";
+      const comment = line.comments.find((c) => c.type === "MANAGEMENT");
       if (comment) {
         comToSet = comment.value;
       }
       this.setState({ oldComment: comToSet });
     } else {
-      this.setState({ oldComment: '' });
+      this.setState({ oldComment: "" });
     }
   }
 
@@ -94,36 +94,44 @@ class TerminatedTable extends React.Component {
   }
 
   surveyListLine(data, survey, handleShow) {
+    const { dataRetreiver } = this.props;
     return (
       <tr key={data.id}>
         <td className="ColCampaign">{survey.label}</td>
-        <td className="ColId" data-testid="campaign-label">{data.id}</td>
+        <td className="ColId" data-testid="campaign-label">
+          {data.id}
+        </td>
         <td className="ColInterviewer">{`${data.interviewer.interviewerLastName} ${data.interviewer.interviewerFirstName}`}</td>
-        <td className="ColFinalizationDate">{`${Utils.convertToDateString(data.finalizationDate)}`}</td>
+        <td className="ColFinalizationDate">{`${Utils.convertToDateString(
+          data.finalizationDate
+        )}`}</td>
         <td className="ColReading">{data.reading ? D.yes : D.no}</td>
         <td className="ColAction">
           <OverlayTrigger
             placement="top"
-            overlay={(
-              <Tooltip>
-                {D.questionnaire}
-              </Tooltip>
-            )}
+            overlay={<Tooltip>{D.questionnaire}</Tooltip>}
           >
             <i
               className="fa fa-calendar EditLink Clickable"
               aria-hidden="true"
-              onClick={() => { window.open(`${this.queenUrl}/queen/readonly/survey-unit/${data.id}`); }}
+              //ici la modification de l'URL vers queen
+              onClick={() => {
+                dataRetreiver.getQuestionnaireModelIdForReviewLink(
+                  [data.id],
+                  (qmIds) => {
+                    const { questionnaireId } = qmIds[0];
+                    window.open(
+                      `${this.queenUrl}/queen/readonly/questionnaire/${questionnaireId}/survey-unit/${data.id}`
+                    );
+                  }
+                );
+              }}
             />
           </OverlayTrigger>
           <span />
           <OverlayTrigger
             placement="top"
-            overlay={(
-              <Tooltip>
-                {D.comment}
-              </Tooltip>
-            )}
+            overlay={<Tooltip>{D.comment}</Tooltip>}
           >
             <i
               className="fa fa-pencil EditCommentSurveyIcon Clickable"
@@ -134,13 +142,15 @@ class TerminatedTable extends React.Component {
           <span />
           <OverlayTrigger
             placement="top"
-            overlay={(
-              <Tooltip>
-                {D.history}
-              </Tooltip>
-            )}
+            overlay={<Tooltip>{D.history}</Tooltip>}
           >
-            <i className="fa fa-history HistoryDisplayIcon Clickable" aria-hidden="true" onClick={(e) => { this.toggleStateHistoryTable(e, data.id); }} />
+            <i
+              className="fa fa-history HistoryDisplayIcon Clickable"
+              aria-hidden="true"
+              onClick={(e) => {
+                this.toggleStateHistoryTable(e, data.id);
+              }}
+            />
           </OverlayTrigger>
         </td>
       </tr>
@@ -149,22 +159,40 @@ class TerminatedTable extends React.Component {
 
   render() {
     const {
-      pagination, displayData, stateData, toggleStateHistory, stateId, showComment,
-      suToModifySelected, oldComment,
+      pagination,
+      displayData,
+      stateData,
+      toggleStateHistory,
+      stateId,
+      showComment,
+      suToModifySelected,
+      oldComment,
     } = this.state;
-    const {
-      data, survey, handleSort, sort,
-    } = this.props;
-    const fieldsToSearch = ['interviewerFirstName', 'interviewerLastName', 'id'];
-    const handleCloseComment = () => { this.handleCloseComment(); };
-    const handleShowComment = (id) => { this.handleShowComment(id); };
-    function handleSortFunct(property) { return () => { handleSort(property); }; }
+    const { data, survey, handleSort, sort } = this.props;
+    const fieldsToSearch = [
+      "interviewerFirstName",
+      "interviewerLastName",
+      "id",
+    ];
+    const handleCloseComment = () => {
+      this.handleCloseComment();
+    };
+    const handleShowComment = (id) => {
+      this.handleShowComment(id);
+    };
+    function handleSortFunct(property) {
+      return () => {
+        handleSort(property);
+      };
+    }
     return (
       <>
         <Row>
           <Col xs="6">
             <PaginationNav.SizeSelector
-              updateFunc={(newPagination) => this.handlePageChange(newPagination)}
+              updateFunc={(newPagination) =>
+                this.handlePageChange(newPagination)
+              }
             />
           </Col>
           <Col xs="6" className="text-right">
@@ -176,13 +204,21 @@ class TerminatedTable extends React.Component {
           </Col>
         </Row>
         <div id="TerminatedTableContainer">
-          <Table id="SurveyList" className="CustomTable" bordered striped hover responsive size="sm">
+          <Table
+            id="SurveyList"
+            className="CustomTable"
+            bordered
+            striped
+            hover
+            responsive
+            size="sm"
+          >
             <thead>
               <tr>
                 <th
                   id="stateHistoryHour"
                   rowSpan="2"
-                  onClick={handleSortFunct('campaignLabel')}
+                  onClick={handleSortFunct("campaignLabel")}
                   className="Clickable ColCampaign"
                 >
                   <SortIcon val="campaignLabel" sort={sort} />
@@ -192,7 +228,7 @@ class TerminatedTable extends React.Component {
                   id="stateHistoryDate"
                   data-testid="TableHeader_id_terminated"
                   rowSpan="2"
-                  onClick={handleSortFunct('id')}
+                  onClick={handleSortFunct("id")}
                   className="Clickable ColId"
                 >
                   <SortIcon val="id" sort={sort} />
@@ -201,7 +237,7 @@ class TerminatedTable extends React.Component {
                 <th
                   id="stateHistoryState"
                   rowSpan="2"
-                  onClick={handleSortFunct('interviewer_terminated')}
+                  onClick={handleSortFunct("interviewer_terminated")}
                   className="Clickable ColInterviewer"
                 >
                   <SortIcon val="interviewer_terminated" sort={sort} />
@@ -209,7 +245,7 @@ class TerminatedTable extends React.Component {
                 </th>
                 <th
                   rowSpan="2"
-                  onClick={handleSortFunct('finalizationDate')}
+                  onClick={handleSortFunct("finalizationDate")}
                   className="Clickable ColFinalizationDate"
                 >
                   <SortIcon val="finalizationDate" sort={sort} />
@@ -217,7 +253,7 @@ class TerminatedTable extends React.Component {
                 </th>
                 <th
                   rowSpan="2"
-                  onClick={handleSortFunct('reading')}
+                  onClick={handleSortFunct("reading")}
                   className="Clickable ColReading"
                 >
                   <SortIcon val="reading" sort={sort} />
@@ -232,15 +268,22 @@ class TerminatedTable extends React.Component {
               {displayData
                 .slice(
                   (pagination.page - 1) * pagination.size,
-                  Math.min(pagination.page * pagination.size, displayData.length),
+                  Math.min(
+                    pagination.page * pagination.size,
+                    displayData.length
+                  )
                 )
-                .map((line) => (
-                  this.surveyListLine(line, survey, () => handleShowComment(line))
-                ))}
+                .map((line) =>
+                  this.surveyListLine(line, survey, () =>
+                    handleShowComment(line)
+                  )
+                )}
             </tbody>
             <Modal show={showComment} onHide={() => handleCloseComment()}>
               <Modal.Header closeButton>
-                <Modal.Title>{D.modifiedCommentSu + suToModifySelected}</Modal.Title>
+                <Modal.Title>
+                  {D.modifiedCommentSu + suToModifySelected}
+                </Modal.Title>
               </Modal.Header>
               <Modal.Body>
                 <Form.Group as={Col} controlId="formGridState">
@@ -249,7 +292,9 @@ class TerminatedTable extends React.Component {
                     type="text"
                     as="textarea"
                     defaultValue={oldComment}
-                    onChange={(e) => this.setState({ newComment: e.target.value })}
+                    onChange={(e) =>
+                      this.setState({ newComment: e.target.value })
+                    }
                   />
                   <Form.Text id="passwordHelpBlock" muted>
                     {D.modifyCommentSuHelpText}
@@ -280,17 +325,20 @@ class TerminatedTable extends React.Component {
           <div className="tableOptionsWrapper">
             <PaginationNav.PageSelector
               pagination={pagination}
-              updateFunc={(newPagination) => { this.handlePageChange(newPagination); }}
+              updateFunc={(newPagination) => {
+                this.handlePageChange(newPagination);
+              }}
               numberOfItems={displayData.length}
             />
           </div>
         </div>
-        {!toggleStateHistory
-        || displayStateHistoryTable(
-          stateData,
-          stateId,
-          (() => this.hideStateHistoryTable()), (() => this.getMaxWidth()),
-        )}
+        {!toggleStateHistory ||
+          displayStateHistoryTable(
+            stateData,
+            stateId,
+            () => this.hideStateHistoryTable(),
+            () => this.getMaxWidth()
+          )}
       </>
     );
   }
