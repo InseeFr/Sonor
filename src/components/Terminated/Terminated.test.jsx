1c3dde42cf544ee0f9125f3dedaa650ee7dc669e
Simon Demazière <simon.demaziere@insee.fr>
2023-10-04 16:21:22 +0200

    fix: update test

--- src/components/Terminated/Terminated.test.jsx
+++ src/components/Terminated/Terminated.test.jsx
@@ -1,41 +1,46 @@
 // Link.react.test.js
-import React from 'react';
-import {
-  render, screen, fireEvent, cleanup,
-} from '@testing-library/react';
-import { Router, Route, Switch } from 'react-router-dom';
-import { createMemoryHistory } from 'history';
-import DataFormatter from '../../utils/DataFormatter';
-import Terminated from './Terminated';
-import mocks from '../../tests/mocks';
+import React from "react";
+import { render, screen, fireEvent, cleanup } from "@testing-library/react";
+import { Router, Route, Switch } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import DataFormatter from "../../utils/DataFormatter";
+import Terminated from "./Terminated";
+import mocks from "../../tests/mocks";
 
 const history = createMemoryHistory();
 
 const toLocaleDateString = Date.prototype.toLocaleString;
 const getHours = Date.prototype.getUTCHours;
 const getMinutes = Date.prototype.getUTCMinutes;
-Date.prototype.toLocaleDateString = function() {
-  return toLocaleDateString.call(this, 'en-EN', { timeZone: 'UTC',year: "numeric", month: "numeric", day: "numeric" });
+Date.prototype.toLocaleDateString = function () {
+  return toLocaleDateString.call(this, "en-EN", {
+    timeZone: "UTC",
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+  });
 };
-Date.prototype.getHours = function() {
+Date.prototype.getHours = function () {
   return getHours.call(this);
 };
-Date.prototype.getMinutes = function() {
+Date.prototype.getMinutes = function () {
   return getMinutes.call(this);
 };
 const OriginalDate = global.Date;
 jest
-  .spyOn(global, 'Date')
-  .mockImplementation((a) => (a ? new OriginalDate(a) : new OriginalDate('2020-08-20T11:01:58.135Z')));
+  .spyOn(global, "Date")
+  .mockImplementation((a) =>
+    a ? new OriginalDate(a) : new OriginalDate("2020-08-20T11:01:58.135Z")
+  );
 Date.now = jest.fn(() => 1597916474000);
 
 beforeEach(() => {
-  history.push('/terminated/vqs2021x00');
+  history.push("/terminated/vqs2021x00");
 });
 
 afterEach(cleanup);
 
-jest.mock('../../utils/DataFormatter');
+jest.mock("../../utils/DataFormatter");
 const survey = mocks.surveyVqs;
 const resp = mocks.suTerminated;
 const { stateHistory } = mocks;
@@ -43,16 +48,21 @@ const { stateHistory } = mocks;
 const TestingRouter = ({ ComponentWithRedirection }) => (
   <Router history={history}>
     <Switch>
-      <Route path="/terminated/:id" render={(routeProps) => <ComponentWithRedirection {...routeProps} />} />
+      <Route
+        path="/terminated/:id"
+        render={(routeProps) => <ComponentWithRedirection {...routeProps} />}
+      />
       <Route
         path="*"
         render={(routeProps) => (
           <div>
-            <div data-testid="Redirect-url">{JSON.stringify(routeProps.history.location.pathname)}</div>
+            <div data-testid="Redirect-url">
+              {JSON.stringify(routeProps.history.location.pathname)}
+            </div>
             <div data-testid="Redirect-survey">
-              {!routeProps.history.location
-              || !routeProps.history.location.survey
-              || JSON.stringify(routeProps.history.location.survey)}
+              {!routeProps.history.location ||
+                !routeProps.history.location.survey ||
+                JSON.stringify(routeProps.history.location.survey)}
             </div>
           </div>
         )}
@@ -62,186 +72,191 @@ const TestingRouter = ({ ComponentWithRedirection }) => (
 );
 
 DataFormatter.mockImplementation(() => ({
-  getListSuTerminated: (id, cb) => (cb(resp)),
-  getStatesSurvey: (id, cb) => (cb(stateHistory)),
+  getListSuTerminated: (id, cb) => cb(resp),
+  getStatesSurvey: (id, cb) => cb(stateHistory),
+  getQuestionnaireModelIdForReviewLink: (ids, cb) => cb("QUESTIONNAIRE"),
 }));
 
 const mockDataRetreiver = new DataFormatter();
 
-it('Component is correctly displayed', async () => {
+it("Component is correctly displayed", async () => {
   const component = render(
     <TestingRouter
-      ComponentWithRedirection={
-        (props) => (
-          <Terminated
-            location={{ survey }}
-            match={props.match}
-            dataRetreiver={mockDataRetreiver}
-          />
-        )
-      }
-    />,
+      ComponentWithRedirection={(props) => (
+        <Terminated
+          location={{ survey }}
+          match={props.match}
+          dataRetreiver={mockDataRetreiver}
+        />
+      )}
+    />
   );
 
   // Should match snapshot (rows displayed)
   expect(component).toMatchSnapshot();
 });
 
-it('Sort by su id', async () => {
+it("Sort by su id", async () => {
   const component = render(
     <TestingRouter
-      ComponentWithRedirection={
-        (props) => (
-          <Terminated
-            location={{ survey }}
-            match={props.match}
-            dataRetreiver={mockDataRetreiver}
-          />
-        )
-      }
-    />,
+      ComponentWithRedirection={(props) => (
+        <Terminated
+          location={{ survey }}
+          match={props.match}
+          dataRetreiver={mockDataRetreiver}
+        />
+      )}
+    />
   );
 
-  screen.getByTestId('TableHeader_id_terminated').click();
+  screen.getByTestId("TableHeader_id_terminated").click();
   // Should match snapshot (rows sorted by id)
   expect(component).toMatchSnapshot();
 });
 
-it('Change page', async () => {
+it("Change page", async () => {
   const component = render(
     <TestingRouter
-      ComponentWithRedirection={
-        (props) => (
-          <Terminated
-            location={{ survey }}
-            match={props.match}
-            dataRetreiver={mockDataRetreiver}
-          />
-        )
-      }
-    />,
+      ComponentWithRedirection={(props) => (
+        <Terminated
+          location={{ survey }}
+          match={props.match}
+          dataRetreiver={mockDataRetreiver}
+        />
+      )}
+    />
   );
 
-  screen.getByTestId('pagination-nav').lastChild.firstChild.click();
+  screen.getByTestId("pagination-nav").lastChild.firstChild.click();
 
   // Should match snapshot (rows displayed have changed)
   expect(component).toMatchSnapshot();
 });
 
-it('Change pagination size', async () => {
+it("Change pagination size", async () => {
   const component = render(
     <TestingRouter
-      ComponentWithRedirection={
-        (props) => (
-          <Terminated
-            location={{ survey }}
-            match={props.match}
-            dataRetreiver={mockDataRetreiver}
-          />
-        )
-      }
-    />,
+      ComponentWithRedirection={(props) => (
+        <Terminated
+          location={{ survey }}
+          match={props.match}
+          dataRetreiver={mockDataRetreiver}
+        />
+      )}
+    />
   );
 
-  fireEvent.change(component.getByTestId('pagination-size-selector'), { target: { value: '10' } });
+  fireEvent.change(component.getByTestId("pagination-size-selector"), {
+    target: { value: "10" },
+  });
 
   // Should match snapshot (all rows are now displayed)
   expect(component).toMatchSnapshot();
 });
 
-it('Select another survey', async () => {
+it("Select another survey", async () => {
   const component = render(
     <TestingRouter
-      ComponentWithRedirection={
-        (props) => (
-          <Terminated
-            location={{ survey }}
-            match={props.match}
-            dataRetreiver={mockDataRetreiver}
-          />
-        )
-      }
-    />,
+      ComponentWithRedirection={(props) => (
+        <Terminated
+          location={{ survey }}
+          match={props.match}
+          dataRetreiver={mockDataRetreiver}
+        />
+      )}
+    />
   );
 
-  const redirectUrl = '/terminated/simpsons2020x00';
+  const redirectUrl = "/terminated/simpsons2020x00";
 
-  fireEvent.change(component.getByTestId('Survey_selector'), { target: { value: 'simpsons2020x00' } });
+  fireEvent.change(component.getByTestId("Survey_selector"), {
+    target: { value: "simpsons2020x00" },
+  });
 
   // Should redirect to '/terminated/simpsons2020x00'
-  expect(screen.getByTestId('Redirect-url').innerHTML).toEqual(`\"${redirectUrl}\"`);
+  expect(screen.getByTestId("Redirect-url").innerHTML).toEqual(
+    `\"${redirectUrl}\"`
+  );
 
   // Location should contain survey object
-  expect(screen.getByTestId('Redirect-survey').innerHTML).not.toEqual('');
-  expect(screen.getByTestId('Redirect-survey')).toMatchSnapshot();
+  expect(screen.getByTestId("Redirect-survey").innerHTML).not.toEqual("");
+  expect(screen.getByTestId("Redirect-survey")).toMatchSnapshot();
 });
 
-it('Reloading the page with no survey set (F5)', async () => {
-  const redirectUrl = '/';
+it("Reloading the page with no survey set (F5)", async () => {
+  const redirectUrl = "/";
   render(
     <TestingRouter
-      ComponentWithRedirection={
-        (props) => (
-          <Terminated
-            location={{ }}
-            match={props.match}
-            dataRetreiver={mockDataRetreiver}
-          />
-        )
-      }
-    />,
+      ComponentWithRedirection={(props) => (
+        <Terminated
+          location={{}}
+          match={props.match}
+          dataRetreiver={mockDataRetreiver}
+        />
+      )}
+    />
   );
 
   // Should redirect to '/'
-  expect(screen.getByTestId('Redirect-url').innerHTML).toEqual(`\"${redirectUrl}\"`);
+  expect(screen.getByTestId("Redirect-url").innerHTML).toEqual(
+    `\"${redirectUrl}\"`
+  );
 });
 
-it('Display and hide history', async () => {
+it("Display and hide history", async () => {
   const component = render(
     <TestingRouter
-      ComponentWithRedirection={
-        (props) => (
-          <Terminated
-            location={{ survey }}
-            match={props.match}
-            dataRetreiver={mockDataRetreiver}
-          />
-        )
-      }
-    />,
+      ComponentWithRedirection={(props) => (
+        <Terminated
+          location={{ survey }}
+          match={props.match}
+          dataRetreiver={mockDataRetreiver}
+        />
+      )}
+    />
   );
 
-  component.baseElement.querySelector('tbody').querySelectorAll('tr')[0].querySelector('.HistoryDisplayIcon').click();
+  component.baseElement
+    .querySelector("tbody")
+    .querySelectorAll("tr")[0]
+    .querySelector(".HistoryDisplayIcon")
+    .click();
 
   // State history table should be displayed
-  expect(component.baseElement.querySelector('#StateHistoryTableContainer')).toBeTruthy();
+  expect(
+    component.baseElement.querySelector("#StateHistoryTableContainer")
+  ).toBeTruthy();
 
   // Component should match snapshot
   expect(component).toMatchSnapshot();
 
-  screen.getByTestId('close-history').click();
+  screen.getByTestId("close-history").click();
 
   // State history should not be displayed anymore
-  expect(component.baseElement.querySelector('#StateHistoryTableContainer')).not.toBeTruthy();
+  expect(
+    component.baseElement.querySelector("#StateHistoryTableContainer")
+  ).not.toBeTruthy();
 });
 
-it('Click on edit', async () => {
+it("Click on edit", async () => {
   const component = render(
     <TestingRouter
-      ComponentWithRedirection={
-        (props) => (
-          <Terminated
-            location={{ survey }}
-            match={props.match}
-            dataRetreiver={mockDataRetreiver}
-          />
-        )
-      }
-    />,
+      ComponentWithRedirection={(props) => (
+        <Terminated
+          location={{ survey }}
+          match={props.match}
+          dataRetreiver={mockDataRetreiver}
+        />
+      )}
+    />
   );
   window.open = jest.fn();
 
-  component.baseElement.querySelector('tbody').querySelectorAll('tr')[0].querySelector('.EditLink').click();
+  component.baseElement
+    .querySelector("tbody")
+    .querySelectorAll("tr")[0]
+    .querySelector(".EditLink")
+    .click();
 
   // window.open should have been called
   expect(window.open).toHaveBeenCalled();
