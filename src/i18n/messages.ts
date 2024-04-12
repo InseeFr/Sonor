be2a8e1dea87939ee7ce8c8eb104e6e9e866246e
Lea Renaux <lea.renaux@insee.fr>
2024-04-12 08:07:54 +0200

    rework getMessages condition

--- src/i18n/messages.ts
+++ src/i18n/messages.ts
@@ -4,10 +4,5 @@ import { messagesFr } from "../i18n-fr";
 export type LocaleType = "fr" | "en";
 
 export const getMessages = (locale: LocaleType) => {
-  switch (locale) {
-    case "fr":
-      return messagesFr;
-    default:
-      return messagesEn;
-  }
+  return locale === "fr" ? messagesFr : messagesEn;
 };
