8ec84d265dd0aec6f51714d01c111a73b350eeb5
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2025-01-31 12:09:47 +0100

    feat: converted DateFormatter to vitest

--- src/utils/DataFormatter.spec.ts
+++ src/utils/DataFormatter.spec.ts
@@ -1,20 +1,19 @@
 import 'core-js';
-import { wait } from '@testing-library/react';
 import DataFormatter from './DataFormatter';
 import Service from './Service';
-import mocks from '../tests/mocks';
 import C from './constants.json';
 import Utils from './Utils';
+import { resolve } from 'dns';
+import mocks from 'tests/mocks';
+import { vi, it, expect } from 'vitest';
 
 const OriginalDate = global.Date;
-jest
-  .spyOn(global, 'Date')
-  .mockImplementation(a =>
-    a ? new OriginalDate(a) : new OriginalDate('2020-08-20T11:01:58.135Z')
-  );
-Date.now = jest.fn(() => 1597916474000);
+vi.spyOn(global, 'Date').mockImplementation(a =>
+  a ? new OriginalDate(a) : new OriginalDate('2020-08-20T11:01:58.135Z')
+);
+Date.now = vi.fn(() => 1597916474000);
 
-jest.mock('./Service');
+vi.mock('./Service');
 
 const {
   mainScreenData,
@@ -36,28 +35,28 @@ const {
 } = mocks;
 
 Service.mockImplementation(() => ({
-  getUser: jest.fn(cb => {
+  getUser: vi.fn(cb => {
     if (cb) {
       cb(userData);
     }
     return Promise.resolve(userData);
   }),
-  putSurveyUnitToValidate: jest.fn((su, cb) => cb({ status: 200 })),
-  putPreferences: jest.fn((prefs, cb) => cb({ status: 200 })),
-  getQuestionnaireId: jest.fn((id, c) => c({ questionnaireId: 'QXT55' })),
-  getCampaigns: jest.fn(cb => cb(mainScreenData)),
-  getInterviewersByCampaign: jest.fn((id, cb) => cb(pearlJamMocks.interviewersByCampaign)),
-  getInterviewers: jest.fn(cb => cb(pearlJamMocks.interviewers)),
-  getSurveyUnitsNotAttributedByCampaign: jest.fn((id, cb) => cb(pearlJamMocks.notAttributed)),
-  getSurveyUnitsAbandonedByCampaign: jest.fn((id, cb) => cb(pearlJamMocks.abandoned)),
-  getTerminatedByCampaign: jest.fn((id, cb) => {
+  putSurveyUnitToValidate: vi.fn((su, cb) => cb({ status: 200 })),
+  putPreferences: vi.fn((prefs, cb) => cb({ status: 200 })),
+  getQuestionnaireId: vi.fn((id, c) => c({ questionnaireId: 'QXT55' })),
+  getCampaigns: vi.fn(cb => cb(mainScreenData)),
+  getInterviewersByCampaign: vi.fn((id, cb) => cb(pearlJamMocks.interviewersByCampaign)),
+  getInterviewers: vi.fn(cb => cb(pearlJamMocks.interviewers)),
+  getSurveyUnitsNotAttributedByCampaign: vi.fn((id, cb) => cb(pearlJamMocks.notAttributed)),
+  getSurveyUnitsAbandonedByCampaign: vi.fn((id, cb) => cb(pearlJamMocks.abandoned)),
+  getTerminatedByCampaign: vi.fn((id, cb) => {
     cb(Utils.sortData(pearlJamMocks.terminated, 'finalizationDate', true));
     resolve(Utils.sortData(processedData, 'finalizationDate', true));
   }),
-  getSurveyUnitsClosable: jest.fn(cb => cb(pearlJamMocks.surveyUnitsClosable)),
-  getStatesBySurveyUnit: jest.fn((id, cb) => cb(pearlJamMocks.states)),
-  getStateCountTotalByCampaign: jest.fn((id, cb) => cb(pearlJamMocks.stateCountTotal)),
-  getSurveyUnits: jest.fn((id, state, cb) => {
+  getSurveyUnitsClosable: vi.fn(cb => cb(pearlJamMocks.surveyUnitsClosable)),
+  getStatesBySurveyUnit: vi.fn((id, cb) => cb(pearlJamMocks.states)),
+  getStateCountTotalByCampaign: vi.fn((id, cb) => cb(pearlJamMocks.stateCountTotal)),
+  getSurveyUnits: vi.fn((id, state, cb) => {
     if (cb) {
       cb(pearlJamMocks.surveyUnits);
     }
@@ -65,7 +64,7 @@ Service.mockImplementation(() => ({
       resolve(pearlJamMocks.surveyUnits);
     });
   }),
-  getStateCountByInterviewer: jest.fn((id, idep, date, cb) => {
+  getStateCountByInterviewer: vi.fn((id, idep, date, cb) => {
     if (cb) {
       cb(pearlJamMocks.interviewerStateCount);
     }
@@ -73,7 +72,7 @@ Service.mockImplementation(() => ({
       resolve(pearlJamMocks.interviewerStateCount);
     });
   }),
-  getCampaignsByInterviewer: jest.fn((idep, cb) => {
+  getCampaignsByInterviewer: vi.fn((idep, cb) => {
     if (cb) {
       cb(pearlJamMocks.campaignsByInterviewer);
     }
@@ -81,9 +80,9 @@ Service.mockImplementation(() => ({
       resolve(pearlJamMocks.campaignsByInterviewer);
     });
   }),
-  getStateCount: jest.fn((id, date, cb) => cb(pearlJamMocks.stateCountTotal)),
-  getStateCountByCampaign: jest.fn((date, cb) => cb(pearlJamMocks.stateCountByCampaign)),
-  getStateCountByInterviewers: jest.fn((date, cb) => {
+  getStateCount: vi.fn((id, date, cb) => cb(pearlJamMocks.stateCountTotal)),
+  getStateCountByCampaign: vi.fn((date, cb) => cb(pearlJamMocks.stateCountByCampaign)),
+  getStateCountByInterviewers: vi.fn((date, cb) => {
     if (cb) {
       cb(pearlJamMocks.stateCountByInterv);
     }
@@ -91,40 +90,40 @@ Service.mockImplementation(() => ({
       resolve(pearlJamMocks.stateCountByInterv);
     });
   }),
-  getSurveyUnitsQuestionnaireIdByCampaign: jest.fn((campaignId, cb) =>
+  getSurveyUnitsQuestionnaireIdByCampaign: vi.fn((campaignId, cb) =>
     cb(pearlJamMocks.surveyUnitsQuestionnaireId)
   ),
 }));
 
 const dataRetreiver = new DataFormatter();
 it('Test getInterviewers', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getInterviewers(callBack);
   // Should return properly formatted data
   expect(callBack).toHaveBeenLastCalledWith(pearlJamMocks.interviewers);
 });
 
 it('Test getUserInfo', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getUserInfo(callBack);
   // Should return properly formatted data
   expect(callBack).toHaveBeenLastCalledWith(userData);
 });
 
 it('Test getPreferences', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getPreferences(callBack);
   // Should return properly formatted data
-  await wait(() => expect(callBack).toHaveBeenLastCalledWith(preferences));
+  await vi.waitFor(() => expect(callBack).toHaveBeenLastCalledWith(preferences));
   // Same when using the returned promise
   const res = await dataRetreiver.getPreferences();
   expect(res).toEqual(preferences);
 });
 
 it('Test updatePreferences', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.updatePreferences(['id'], callBack);
-  await wait(() => expect(callBack).toHaveBeenCalled());
+  await vi.waitFor(() => expect(callBack).toHaveBeenCalled());
   // Should return properly formatted data
   expect(callBack).toHaveBeenLastCalledWith({ status: 200 });
   // Same when using the returned promise
@@ -133,37 +132,37 @@ it('Test updatePreferences', async () => {
 });
 
 it('Test getDataForCampaignPortal', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getDataForCampaignPortal('id', callBack);
-  await wait(() => expect(callBack).toHaveBeenCalled());
+  await vi.waitFor(() => expect(callBack).toHaveBeenCalled());
   // Should return properly formatted data
   expect(callBack).toHaveBeenLastCalledWith(campaignPortalData2);
 });
 
 it('Test getDataForMainScreen', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getDataForMainScreen(null, callBack);
   // Should return properly formatted data
   expect(callBack).toHaveBeenLastCalledWith(formattedSurveys);
 });
 
 it('Test getDataForClosePage', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getDataForClosePage(callBack);
   // Should return properly formatted data
   expect(callBack).toHaveBeenLastCalledWith(pearlJamMocks.surveyUnitsClosable);
 });
 
 it('Test getDataForListSU', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getDataForListSU('id', callBack);
-  await wait(() => expect(callBack).toHaveBeenCalled());
+  await vi.waitFor(() => expect(callBack).toHaveBeenCalled());
   // Should return properly formatted data
   expect(callBack).toHaveBeenLastCalledWith(formattedListSuData);
 });
 
 it('Test getDataForReview', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getDataForReview(null, callBack, [
     ...mainScreenData,
     {
@@ -184,7 +183,7 @@ it('Test getDataForReview', async () => {
       preference: true,
     },
   ]);
-  await wait(() => expect(callBack).toHaveBeenCalled());
+  await vi.waitFor(() => expect(callBack).toHaveBeenCalled());
   // Should return properly formatted data
   expect(callBack.mock.calls[0][0].map(a => a.id).sort()).toEqual(
     formattedReviewData.map(a => a.id).sort()
@@ -192,15 +191,15 @@ it('Test getDataForReview', async () => {
 });
 
 it('Test getListSuTerminated', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getListSuTerminated('id', callBack);
-  await wait(() => expect(callBack).toHaveBeenCalled());
+  await vi.waitFor(() => expect(callBack).toHaveBeenCalled());
   // Should return properly formatted data
   expect(callBack).toHaveBeenLastCalledWith(formattedSuTerminated);
 });
 
 it('Test getDataForMonitoringTable (by interviewer 1 survey)', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getDataForMonitoringTable(
     surveyVqs,
     new Date('2020-08-20T11:01:58.135Z'),
@@ -208,13 +207,13 @@ it('Test getDataForMonitoringTable (by interviewer 1 survey)', async () => {
     C.BY_INTERVIEWER_ONE_SURVEY,
     callBack
   );
-  await wait(() => expect(callBack).toHaveBeenCalled());
+  await vi.waitFor(() => expect(callBack).toHaveBeenCalled());
   // Should return properly formatted data
   expect(callBack).toHaveBeenLastCalledWith(formattedDataMonitoringTableByInterv1surv);
 });
 
 it('Test getDataForMonitoringTable (by survey 1 Interviewer)', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getDataForMonitoringTable(
     mainScreenData1Survey,
     new Date('2020-08-20T11:01:58.135Z'),
@@ -222,13 +221,13 @@ it('Test getDataForMonitoringTable (by survey 1 Interviewer)', async () => {
     C.BY_SURVEY_ONE_INTERVIEWER,
     callBack
   );
-  await wait(() => expect(callBack).toHaveBeenCalled());
+  await vi.waitFor(() => expect(callBack).toHaveBeenCalled());
   // Should return properly formatted data
   expect(callBack).toHaveBeenCalledWith(formattedDataMonitoringTableByInterv);
 });
 
 it('Test getDataForMonitoringTable (by survey)', async () => {
-  const callBack2 = jest.fn();
+  const callBack2 = vi.fn();
   const dataRet = new DataFormatter();
   dataRet.getDataForMonitoringTable(
     mainScreenData1Survey,
@@ -238,13 +237,13 @@ it('Test getDataForMonitoringTable (by survey)', async () => {
     callBack2,
     mainScreenData
   );
-  await wait(() => expect(callBack2).toHaveBeenCalled());
+  await vi.waitFor(() => expect(callBack2).toHaveBeenCalled());
   // Should return properly formatted data
   expect(callBack2).toHaveBeenCalledWith(formattedDataMonitoringTableBysurvey);
 });
 
 it('Test getDataForMonitoringTable (by site)', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getDataForMonitoringTable(
     formattedSurveys,
     new Date('2020-08-20T11:01:58.135Z'),
@@ -252,15 +251,15 @@ it('Test getDataForMonitoringTable (by site)', async () => {
     C.BY_SITE,
     callBack
   );
-  await wait(() => expect(callBack).toHaveBeenCalled());
+  await vi.waitFor(() => expect(callBack).toHaveBeenCalled());
   // Should return properly formatted data
   expect(callBack).toHaveBeenLastCalledWith(formattedDataMonitoringTableBySite);
 });
 
 it('Test getStatesSurvey', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.getStatesSurvey('id', callBack);
-  await wait(() => expect(callBack).toHaveBeenCalled());
+  await vi.waitFor(() => expect(callBack).toHaveBeenCalled());
   // Should return properly formatted data
   expect(callBack).toHaveBeenLastCalledWith(pearlJamMocks.states.states);
   // Same when using the returned promise
@@ -269,9 +268,9 @@ it('Test getStatesSurvey', async () => {
 });
 
 it('Test finalizeSurveyUnits', async () => {
-  const callBack = jest.fn();
+  const callBack = vi.fn();
   dataRetreiver.finalizeSurveyUnits(['id'], callBack);
-  await wait(() => expect(callBack).toHaveBeenCalled());
+  await vi.waitFor(() => expect(callBack).toHaveBeenCalled());
   // Should return properly formatted data
   expect(callBack).toHaveBeenLastCalledWith([{ status: 200 }]);
   // Same when using the returned promise
