972f0d7c01f6bc8fc99b735e8b60a425382b31d6
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2025-02-03 09:23:32 +0100

    fix: set default dev server

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { resolve } from 'node:url';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   test: {
     globals: true,
     include: ['src/**/*.spec.*'],
