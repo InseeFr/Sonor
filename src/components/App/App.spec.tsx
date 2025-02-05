08f71ce810816288f8f2e60a5fda3a1de9d19da8
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2025-02-05 16:32:54 +0100

    fix: add timeout

--- src/components/App/App.spec.tsx
+++ src/components/App/App.spec.tsx
@@ -4,7 +4,7 @@ import { mockOidcForUser, mockOidcFailed } from '../CustomHooks/useAuth';
 import { describe, expect, it } from 'vitest';
 import D from '../../i18n';
 
-describe('Component display', () => {
+describe('Component display', { timeout: 30000 }, () => {
   it('Component is displayed ', async () => {
     mockOidcForUser();
 
@@ -12,7 +12,7 @@ describe('Component display', () => {
       render(<App />);
     });
 
-    expect(await screen.findByText(D.surveyList));
+    expect(await screen.findByText(D.surveyList, {}, { timeout: 30000 }));
   });
 
   it('Component is not displayed ', async () => {
