d5e6e40bd4c417f0a7a71aaccacbc402354faa7b
Pierre-Raphaël Wozny <pierre-raphael.wozny@insee.fr>
2025-02-05 14:16:31 +0100

    fix: wait for text display instead of using timeouts

--- src/components/App/App.spec.tsx
+++ src/components/App/App.spec.tsx
@@ -1,18 +1,19 @@
-import { screen, render, act } from '@testing-library/react';
+import { screen, render, act, waitFor } from '@testing-library/react';
 import { App } from './App';
 import { mockOidcForUser, mockOidcFailed } from '../CustomHooks/useAuth';
 import { describe, expect, it } from 'vitest';
 import D from '../../i18n';
 
 describe('Component display', () => {
-  it('Component is displayed ', { timeout: 30000 }, async () => {
+  it('Component is displayed ', async () => {
     mockOidcForUser();
 
     await act(async () => {
       render(<App />);
     });
 
-    expect(await screen.findByText(D.surveyList, {}, { timeout: 20000 }));
+    await waitFor(() => screen.getByText(D.surveyList));
+    expect(await screen.findByText(D.surveyList));
   });
 
   it('Component is not displayed ', async () => {
