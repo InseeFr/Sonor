b6536263fa9a5e51dc3121ab3109f74b3cd9ebe0
Simon Demaziere <simon.demaziere@insee.fr>
2025-03-06 14:46:03 +0100

    fix: contact outcome label update

--- src/i18n/labels/labels_contactOutcomes.js
+++ src/i18n/labels/labels_contactOutcomes.js
@@ -12,12 +12,12 @@ const labelsContactOutcomes = {
     fr: 'Refus',
   },
   ALA: {
-    en: 'Already answered (other mode)',
-    fr: 'Déjà répondu (autre mode)',
+    en: 'Already answered (other survey)',
+    fr: 'Déjà répondu (autre enquête)',
   },
   UCD: {
     en: 'Unusable Contact Data',
-    fr: 'Données de contact inutilisables (tél, mail)',
+    fr: 'Données de contact inutilisables',
   },
   UTR: {
     en: 'Unable To Respond',
@@ -33,7 +33,7 @@ const labelsContactOutcomes = {
   },
   NUH: {
     en: 'No longer Used for Habitation',
-    fr: 'Logement ayant perdu son usage d\'habitation',
+    fr: "Logement ayant perdu son usage d'habitation",
   },
   NER: {
     en: 'No interview for Exceptional Reasons',
